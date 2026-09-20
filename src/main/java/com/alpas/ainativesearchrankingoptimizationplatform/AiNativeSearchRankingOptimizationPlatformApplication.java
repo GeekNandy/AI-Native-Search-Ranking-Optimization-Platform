d248cca96041f2e40a403a18d721784eb27ceae6
Sharad Nanda <5526742+GeekNandy@users.noreply.github.com>
2026-09-20 22:15:11 +0200

    Project Initiation

---
+++ src/main/java/com/alpas/ainativesearchrankingoptimizationplatform/AiNativeSearchRankingOptimizationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.alpas.ainativesearchrankingoptimizationplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiNativeSearchRankingOptimizationPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiNativeSearchRankingOptimizationPlatformApplication.class, args);
+    }
+
+}
